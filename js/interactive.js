3d2b978e5e10cf65a5a2bd90ebb3b867e1ec124e
exzolink <exzolink@gmail.com>
2020-02-04 13:51:47 +0300

    fix

--- js/interactive.js
+++ js/interactive.js
@@ -260,10 +260,46 @@ $(document).on('click', '.sm-li_main', function (e) {
     slidesToScroll: 2,
     slidesToShow: 2,
     arrows: false,
-    dots: true,
+    dots: false,
     responsive: [
     {
-      breakpoint: 960,
+      breakpoint: 1160,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
+    });
+  };
+});
+
+$(function() {
+    if (width < 1169) {
+    $('.sponsors-botblock').slick({
+    infinite: true,
+    slidesToScroll: 2,
+    slidesToShow: 2,
+    arrows: false,
+    dots: true,
+    asNavFor: '.sponsors-upblock',
+    responsive: [
+    {
+      breakpoint: 1160,
       settings: {
         slidesToShow: 4,
         slidesToScroll: 4,
